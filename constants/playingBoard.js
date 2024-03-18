299ba8a02afca752927478e8206a6e203dcb4c77
manfredwern <manfredwern@users.noreply.github.com>
2024-03-18 16:10:00 +0000

    deploy: 669e86465745ad3119c3e6fb1ea18e05a3440c99

--- constants/playingBoard.js
+++
@@ -1,9 +0,0 @@
-const MAX_CELL_COUNT = 9;
-
-export function generateBoardCellsData() {
-    return Array.from({ length: MAX_CELL_COUNT }, (_, index) => ({
-        cellId: index + 1,
-        player: "",
-        className: ""
-    }));
-}
