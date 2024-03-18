45e0ab9780b8bd9651ded285f6af664f8489d1f0
fredo <fredowern>
2024-03-18 10:14:35 +0100

    refactor codebase; adding constants and better function names

--- constants/playingBoard.js
+++ constants/playingBoard.js
@@ -4,6 +4,6 @@ export function generateBoardCellsData() {
     return Array.from({ length: MAX_CELL_COUNT }, (_, index) => ({
         cellId: index + 1,
         player: "",
-        class: ""
+        className: ""
     }));
 }
